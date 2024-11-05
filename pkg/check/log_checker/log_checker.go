767e935aa9475f2483c243c626894bcea4d3759d
huanghj78 <1353188493@qq.com>
2024-11-05 08:12:18 +0000

    todo: add new checker

---
+++ pkg/check/log_checker/log_checker.go
@@ -0,0 +1,14 @@
+package logchecker
+
+import "github.com/huanghj78/jepsenFuzz/pkg/core"
+
+type Checker struct{}
+
+func (Checker) Check(_ core.Model, _ []core.Operation) (bool, error) {
+
+	return true, nil
+}
+
+func (Checker) Name() string {
+	return "log_checker"
+}
